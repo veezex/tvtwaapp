39a98bee165392584d4b02289d8a386b10b71095
vzxw <vlad@drewl.com>
2025-10-27 18:16:30 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@ import TradingViewWidget from "./components/TradingViewWidget.vue";
 
 const urlParams = new URLSearchParams(window.location.search);
 const startParam = urlParams.get("tgWebAppStartParam") || undefined;
+const startObject = JSON.parse(startParam ? atob(startParam) : "{}") as {
+  symbol?: string;
+};
 </script>
 
 <template>
-  <TradingViewWidget :symbol="startParam" class="w-dvw h-dvh bg-gray-700" />
+  <TradingViewWidget
+    :symbol="startObject.symbol"
+    class="w-dvw h-dvh bg-gray-700"
+  />
 </template>
