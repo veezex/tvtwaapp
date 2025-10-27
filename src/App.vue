f23e443fa9f4460140a6cd6f7914f3dbee6d14fd
vzxw <vlad@drewl.com>
2025-10-27 15:45:22 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const startParam = ref<string | null>(null);
+
+onMounted(() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  startParam.value = urlParams.get("tgWebAppStartParam");
+});
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello</h1>
+  <p v-if="startParam">Указанный параметр: {{ startParam }}</p>
 </template>
